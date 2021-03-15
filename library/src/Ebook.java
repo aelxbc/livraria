77e3a83a04ce33ebe19481d4d04cc3b3ef9ea821
VINICIUS DA SILVA BRANDAO <48166738+Brandaovini@users.noreply.github.com>
2021-03-14 21:28:03 -0300

    Add files via upload

---
+++ library/src/Ebook.java
@@ -0,0 +1,15 @@
+public class Ebook extends Livro {
+
+    private String marcaDagua;
+
+    public Ebook(Autor autor) {
+        super(autor);
+    }
+    public void setMarcaDagua(String marcaDagua){
+        this.marcaDagua = marcaDagua;
+    }
+    public String getMarcaDagua(){
+        return this.marcaDagua;
+    }
+
+}
